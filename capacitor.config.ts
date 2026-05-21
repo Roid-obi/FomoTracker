b802fac8211dea0f7da3ad26c20a55aaf24e664a
NeyShiKu <souls1771@gmail.com>
2026-05-21 16:14:06 +0700

    feat: add web extension project and android project using capacitor.js

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fomotracker.app',
+  appName: 'Fomo Tracker',
+  webDir: 'out'
+};
+
+export default config;

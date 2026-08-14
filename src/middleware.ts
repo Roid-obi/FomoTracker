4bb7b992799844f013f797a5b6d324025756287a
Roid-obi <roidrobih@gmail.com>
2026-08-14 17:30:15 +0700

    fix: mengganti getUser() menjadi getSession()

--- src/middleware.ts
+++ src/middleware.ts
@@ -65,8 +65,9 @@ export async function middleware(request: NextRequest) {
   });
 
   const {
-    data: { user },
-  } = await supabase.auth.getUser();
+    data: { session },
+  } = await supabase.auth.getSession();
+  const user = session?.user;
 
   const isAuthPage = request.nextUrl.pathname.startsWith("/auth");
   const isAppPage =
